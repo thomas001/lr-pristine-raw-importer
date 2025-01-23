e0c55b20baa64bdd1cf691220b11bb66569f764d
Thomas Weidner <thomas001le@gmail.com>
2025-01-23 23:34:29 +0100

    Add a version setting populated by git.

--- lr-pristine-raw-importer.lrplugin/Settings.lua
+++ lr-pristine-raw-importer.lrplugin/Settings.lua
@@ -5,4 +5,5 @@ return {
     TriggerFileName = "dxo-trigger-PureRawv4",
     ImportFileName = "dxo-imported-files-PureRawv4",
     PluginDescription = "Pristine RAW Importer is a Lightroom plugin for importing photos processed with DxO PureRAW4.",
+    Version = "%%GITHUB_REF_NAME%%",
 }
